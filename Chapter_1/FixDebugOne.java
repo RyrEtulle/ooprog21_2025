93435c1a6e7491ee303c9417a58723dbe73e49d6
RyrEtulle-UCM <ryretulle@gmail.com>
2025-08-31 17:07:09 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,19 @@
+public class FixDebugOne
+{
+   
+      public static void main(String[] args)   
+   {
+      System.out.println("Hello World!");         
+   }
+}
+/*
+List down the spotted bugs below:
+
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+
+Bugs:
+1. (input enteries here...)
+2. ....
+
+*/

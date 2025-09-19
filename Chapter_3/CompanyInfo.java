c1d44339139610aa095427014c54edc308c3f1d8
RyrEtulle-UCM <ryretulle@gmail.com>
2025-09-19 09:30:31 +0800

    Add files via upload

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,23 @@
+
+public class CompanyInfo {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Company Name: Tech Solutions Inc.");
+        
+        
+        displayHours();
+    }
+
+    
+    public static void displayHours() {
+        System.out.println("\nBusiness Hours:");
+        System.out.println("Monday:    9:00 AM - 6:00 PM");
+        System.out.println("Tuesday:   9:00 AM - 6:00 PM");
+        System.out.println("Wednesday: 9:00 AM - 6:00 PM");
+        System.out.println("Thursday:  9:00 AM - 6:00 PM");
+        System.out.println("Friday:    9:00 AM - 6:00 PM");
+        System.out.println("Saturday:  10:00 AM - 4:00 PM");
+        System.out.println("Sunday:    Closed");
+    }
+}
